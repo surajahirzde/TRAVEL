f8e9bd6b34485072b985a8a9d6165a72326cd37f
surajahirzde <sy241527@gmail.com>
2025-12-26 14:36:45 +0530

    frfg

--- src/Helper/Navbar.jsx
+++ src/Helper/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
 
         {/* Merchant Profile Section */}
         <div className="navbar-profile-section">
-          <div className="navbar-merchant-info">
+          <div className="navbar-merchant-info">   
             <span className="navbar-merchant-label">Welcome</span>
             <span className="navbar-merchant-id">Sir / mam </span>
           </div>
